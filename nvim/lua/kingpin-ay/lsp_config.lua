89a93daa6b8205b0a85dc805d8d9edaeffcbf598
kingpin-ay <ayushmondal001@gmail.com>
2023-08-09 13:01:20 +0530

    adding tsserver as the lsp setup

--- nvim/lua/kingpin-ay/lsp_config.lua
+++ nvim/lua/kingpin-ay/lsp_config.lua
@@ -1,6 +1,7 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  esure_installed = {"lua_ls"}
+  esure_installed = {"lua_ls" , "tsserver"}
 })
 
 require("lspconfig").lua_ls.setup {}
+require("lspconfig").tsserver.setup {}
