f9bbac12c6b736c4e1f36306879c91a4ddc1c006
kingpin-ay <ayushmondal001@gmail.com>
2023-08-09 23:58:19 +0530

    added the functionility for not serching through node modules

--- nvim/lua/kingpin-ay/null-ls.lua
+++ nvim/lua/kingpin-ay/null-ls.lua
@@ -1,7 +1,8 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
+  autostart = true,
+  sources = {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.eslint,
     },
--- nvim/lua/kingpin-ay/telescope.lua
+++ nvim/lua/kingpin-ay/telescope.lua
@@ -2,6 +2,11 @@ require('telescope').setup{
   defaults = {
     -- Default configuration for telescope goes here:
     -- config_key = value,
+    file_ignore_patterns = {
+      "node_modules",
+      "dist",
+      "test",
+    },
     mappings = {
       i = {
         -- map actions.which_key to <C-h> (default: <C-/>)
