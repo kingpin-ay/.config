7a168344fb7a7f7e530c0699e194c4d69820244d
kingpin-ay <ayushmondal001@gmail.com>
2023-08-15 11:18:03 +0530

    nvimtree added to the lowcost

--- nvim/lua/lowcost/options.lua
+++ nvim/lua/lowcost/options.lua
@@ -33,3 +33,9 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
--- nvim/lua/lowcost/plugins.lua
+++ nvim/lua/lowcost/plugins.lua
@@ -41,6 +41,17 @@ require('lazy').setup({
   },
 
   {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
+  {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = {
