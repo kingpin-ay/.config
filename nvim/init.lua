b46043b471f7991b98164629d866b9bedddd04a1
kingpin-ay <ayushmondal001@gmail.com>
2023-08-15 10:57:05 +0530

    kickstart one

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,25 @@
-require('kingpin-ay.options')
-require('kingpin-ay.keymaps')
-require('kingpin-ay.plugins')
-require('kingpin-ay.colorscheme')
-require('kingpin-ay.cmp')
-require('kingpin-ay.lsp_config')
-require('kingpin-ay.telescope')
-require("kingpin-ay.autopairs")
-require('kingpin-ay.treesitter')
-require("kingpin-ay.nvimtree")
-require("kingpin-ay.gitsigns")
-require("kingpin-ay.comment")
-require("kingpin-ay.toggleterm")
-require("kingpin-ay.null-ls")
+--[[ require('kingpin-ay.options') ]]
+--[[ require('kingpin-ay.keymaps') ]]
+--[[ require('kingpin-ay.plugins') ]]
+--[[ require('kingpin-ay.colorscheme') ]]
+--[[ require('kingpin-ay.cmp') ]]
+--[[ require('kingpin-ay.lsp_config') ]]
+--[[ require('kingpin-ay.telescope') ]]
+--[[ require("kingpin-ay.autopairs") ]]
+--[[ require('kingpin-ay.treesitter') ]]
+--[[ require("kingpin-ay.nvimtree") ]]
+--[[ require("kingpin-ay.gitsigns") ]]
+--[[ require("kingpin-ay.comment") ]]
+--[[ require("kingpin-ay.toggleterm") ]]
+--[[ require("kingpin-ay.null-ls") ]]
+--
+--
+
+
+require('lowcost.plugins')
+require('lowcost.telescope')
+require('lowcost.treesitter')
+require('lowcost.keymap')
+require('lowcost.lspconf')
+require('lowcost.options')
+
