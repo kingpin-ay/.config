feadacf21dc08b23ac4744979dad6a94f01188a6
kingpin-ay <ayushmondal001@gmail.com>
2023-08-19 23:55:58 +0530

    added some extra keymaps and functionility to the lowcost one

--- nvim/lua/lowcost/options.lua
+++ nvim/lua/lowcost/options.lua
@@ -2,7 +2,9 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
-
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
